325b30f0ac9221c835463a1636c74ab65ae3f5ac
Alvid Nguyen <d.fed22@gsoft01.tk>
2022-08-16 22:34:07 +0700

    creating header component

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,5 +1,28 @@
+import { Grid, Container } from "@mui/material";
 import React from "react";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div id="header">
+      <Container maxWidth={"xl"}>
+        <Grid container>
+          {/* header -- logo and search bar */}
+          <Grid item xs={3}>
+            <div className="header__logo">logo</div>
+            <div className="header__search">search</div>
+          </Grid>
+
+          {/* header -- tags */}
+          <Grid item xs={6}>
+            <div className="header__tags">tags</div>
+          </Grid>
+
+          {/* header -- account */}
+          <Grid item xs={3}>
+            <div className="header__account">account</div>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
 }
