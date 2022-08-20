d11c3073574dca69f7c141e9a7718219652d41ef
Alvid Nguyen <d.fed22@gsoft01.tk>
2022-08-20 23:27:48 +0700

    complete header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { createTheme, ThemeProvider } from "@mui/material";
+import { createTheme, Grid, ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import Routers from "./routers";
 import Header from "./components/Header";
+import { Container } from "@mui/system";
 
 export default function App() {
   const theme = createTheme({
     palette: {
       primary: {
         main: "#fc4a1a",
+      },
+      secondary: {
+        main: "#212121",
       },
     },
   });
@@ -17,7 +21,16 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Header />
-        <Routers />
+        <Container id="Layout" maxWidth="xl">
+          <Grid container>
+            <Grid item xs={4}>
+              Sidebar
+            </Grid>
+            <Grid item xs={8}>
+              <Routers />
+            </Grid>
+          </Grid>
+        </Container>
       </BrowserRouter>
     </ThemeProvider>
   );
--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,28 +1,69 @@
-import { Grid, Container } from "@mui/material";
+import { ModeComment, Notifications } from "@mui/icons-material";
+import {
+  AppBar,
+  Autocomplete,
+  Avatar,
+  Badge,
+  IconButton,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
 
 export default function Header() {
+  let options = [
+    {
+      label: "HTML",
+      value: 0,
+    },
+    {
+      label: "CSS",
+      value: 1,
+    },
+    {
+      label: "JS",
+      value: 2,
+    },
+  ];
+
   return (
-    <div id="header">
-      <Container maxWidth={"xl"}>
-        <Grid container>
-          {/* header -- logo and search bar */}
-          <Grid item xs={3}>
-            <div className="header__logo">logo</div>
-            <div className="header__search">search</div>
-          </Grid>
+    <AppBar id="header" sx={{ background: "#fff" }}>
+      <Toolbar>
+        <Typography variant="h3" component="h1" color="primary" marginRight={2}>
+          Devnet
+        </Typography>
 
-          {/* header -- tags */}
-          <Grid item xs={6}>
-            <div className="header__tags">tags</div>
-          </Grid>
+        <Autocomplete
+          sx={{ width: 500, margin: "auto" }}
+          options={options}
+          renderInput={(params) => <TextField {...params} placeholder="Search"></TextField>}
+        />
 
-          {/* header -- account */}
-          <Grid item xs={3}>
-            <div className="header__account">account</div>
-          </Grid>
-        </Grid>
-      </Container>
-    </div>
+        <Box sx={{ flexGrow: 1 }} />
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <IconButton size="large" aria-label="show 4 new mails" color="secondary">
+            <Badge badgeContent={4} color="error">
+              <ModeComment />
+            </Badge>
+          </IconButton>
+          <IconButton size="large" aria-label="show 17 new notifications" color="secondary">
+            <Badge badgeContent={17} color="error">
+              <Notifications />
+            </Badge>
+          </IconButton>
+          <IconButton
+            size="large"
+            edge="end"
+            aria-label="account of current user"
+            aria-haspopup="true"
+            color="secondary"
+          >
+            <Avatar src="https://img.freepik.com/free-photo/cheerful-beautiful-young-asian-woman-feeling-happy-smiling-camera-while-traveling-chinatown-beijing-china_7861-1341.jpg?w=1060&t=st=1661008650~exp=1661009250~hmac=0d1a8ad61ae71be0172b70daf240f3bae507c215cfeff8f0439ada91bd87a525"></Avatar>
+          </IconButton>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 }
